51b53536a0081434cfff8d0bacb83f325742f668
Lynelria <lynaria.kel@gmail.com>
2015-05-08 22:23:08 +0200

    Fix the address to use to launch the nodeJS server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
-var server = app.listen(server_port, function () {
+var server = app.listen(server_port, server_ip_address, function () {
 
     var host = server.address().address;
     var port = server.address().port;
