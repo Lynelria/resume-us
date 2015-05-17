7a807bcec9616d7508553e27c12fc6f7f3fe9db6
Lynelria <lynaria.kel@gmail.com>
2015-05-17 17:02:45 +0200

    Clean the image

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     notify = require('gulp-notify'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    UglifyJS = require("uglify-js");;
 
 
 var config = {
@@ -36,3 +37,18 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['bower', 'icons', 'css']);
+
+
+gulp.task('uglify', function () {
+    return result = UglifyJS.minify(["bower_components/jquery/dist/jquery.min.js",
+                                "bower_components/bootstrap/dist/js/bootstrap.min.js",
+                                "bower_components/angular/angular.min.js",
+                                "bower_components/angular-route/angular-route.min.js",
+                                "bower_components/angular-sanitize/angular-sanitize.min.js",
+                                "bower_components/angular-bootstrap/ui-bootstrap.min.js",
+                                "bower_components/waypoints/lib/jquery.waypoints.js",
+                                "public/js/app.js",
+                                "public/js/controllers.js"], {
+        output: "public/js/uglify.min.js"
+    });
+});
