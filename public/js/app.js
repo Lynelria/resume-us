1a96b9e17ee47fa4dc1cd77d575fc33002fb9097
Lynelria <lynaria.kel@gmail.com>
2015-05-17 17:31:20 +0200

    Last fix before push in prod

--- public/js/app.js
+++ public/js/app.js
@@ -17,3 +17,12 @@ resumeApp.config(['$routeProvider',
             redirectTo: '/projects'
         });
   }]);
+
+
+resumeApp.run(function ($rootScope, $location, $anchorScroll, $routeParams) {
+    //when the route is changed scroll to the proper element.
+    $rootScope.$on('$routeChangeSuccess', function (newRoute, oldRoute) {
+        $location.hash($routeParams.scrollTo);
+        $anchorScroll();
+    });
+});
